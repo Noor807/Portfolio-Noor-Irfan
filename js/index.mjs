24283350863b98a40630565de8a11aade1fd919b
Noor807 <152153984+Noor807@users.noreply.github.com>
2024-10-03 22:29:15 +0200

    save
    create html of different tags

--- js/index.mjs
+++ js/index.mjs
@@ -1,16 +1,21 @@
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablinks of tablinks){
-        tablinks.classList.remove("active-link");
-
+function opentab(tabname, event){
+    // Remove active class from all tab links
+    for (let link of tablinks) {
+        link.classList.remove("active-link");
     }
-    for(tabcontents of tabcontents){
-        tabcontents.classList.remove("active-tab");
-
-    }
-    Event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
     
+    // Remove active class from all tab contents
+    for (let content of tabcontents) {
+        content.classList.remove("active-tab");
+    }
+    
+    // Add active class to the clicked tab link
+    event.currentTarget.classList.add("active-link");
+    
+    // Show the corresponding tab content
+    document.getElementById(tabname).classList.add("active-tab");
 }
